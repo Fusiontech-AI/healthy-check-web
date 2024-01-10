dd849c6ed5f4bac73984865bd021438f69c1cb0c
fengdanyue <549721719@qq.com>
2024-01-10 11:21:28 +0800

    feat:团检任务审核页面绘制

--- src/views/tool/gen/index.vue
+++ src/views/tool/gen/index.vue
@@ -89,7 +89,7 @@
           </template>
         </el-table-column>
       </el-table>
-      <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
+      <paginations v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
     </el-card>
 
     <!-- 预览界面 -->
