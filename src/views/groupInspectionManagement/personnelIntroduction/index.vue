1456b11cbf5927a8d8b8890dad1426b71a333537
fengdanyue <549721719@qq.com>
2024-01-11 20:11:50 +0800

    feat：团检管理样式修改

--- src/views/groupInspectionManagement/personnelIntroduction/index.vue
+++ src/views/groupInspectionManagement/personnelIntroduction/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <el-row :gutter="8">
-      <el-col :span="6">
+  <div class="text-[14px]">
+    <el-row>
+      <el-col :span="5">
         <el-card>
           <el-select class="w-full my-2" placeholder="请输入">
             <el-option label="全部" value=""></el-option>
@@ -12,19 +12,23 @@
             <el-button>重置</el-button>
           </div>
           <div class="list_card">
-            <div
-              v-for="item in 20"
-              :key="item"
-              class="h-[120px] w-full bg-gray-100 hover:bg-sky-200 p-2 flex flex-col justify-between mb-2 cursor-pointer"
-            >
-              <div class="flex justify-between items-center"><span>体检任务：2023年职业病体检</span><span>健</span></div>
-              <div class="flex justify-between items-center"><span>单位：胖胖乐有限公司</span><span>待审</span></div>
-              <div class="flex justify-between items-center"><span>签订日期：2023-8-21</span><span>未通过</span></div>
-            </div>
+            <el-card shadow="hover" v-for="item in 20" :key="item" class="card_item" :class="isActive == item?'active':''">
+              <div class="flex justify-between items-center">
+                <span class="tetx-[#141C28]">2023年职业病体检</span>
+                <div class="flex">
+                  <span class="ml-auto px-[3px] rounded-[2px] font-bold text-[#fff] bg-[#FFA81C]">放</span>
+                  <span class="ml-1 px-[3px] rounded-[2px] font-bold text-[#fff] bg-[#FFA81C]">健</span>
+                </div>
+              </div>
+              <div class="flex justify-between items-center my-2 text-[12px] text-[#89919F]"><span>胖胖乐有限公司</span></div>
+              <div class="flex justify-between items-center text-[12px] text-[#89919F]">
+                <span>2023-8-21</span><span class="text-red">未通过</span>
+              </div>
+            </el-card>
           </div>
         </el-card>
       </el-col>
-      <el-col :span="18">
+      <el-col :span="19">
         <el-card class="content">
           <div class="flex justify-end">
             <el-button type="primary">下载模板</el-button>
@@ -34,10 +38,10 @@
           </div>
           <div>
             <div class="my-2 ">
-              <div class="font-bold"><span></span>基本信息</div>
+              <div class="font-bold card_title"><span></span>基本信息</div>
             </div>
             <el-form :model="formValue" :rules="rules" :disabled="false" :label-width="80">
-              <Grid ref="gridRef" :gap="12" :cols="4">
+              <Grid ref="gridRef" :gap="12" :cols="3">
                 <GridItem v-for="(item, index) in formColumns" :key="item.prop" :index="index">
                   <el-form-item :label="item.label" :prop="item.prop">
                     <SearchFormItem :column="item" :search-param="formValue" />
@@ -45,15 +49,15 @@
                 </GridItem>
               </Grid>
             </el-form>
-            <SearchForm :columns="formColumns" :search-param="formValue" :search-col="4" :showSearchBtn="false" />
+            <SearchForm :columns="formColumns" :search-param="formValue" :search-col="3" />
           </div>
           <div>
             <div class="my-2">
-              <div class="font-bold"><span></span>人员信息</div>
+              <div class="font-bold card_title"><span></span>人员信息</div>
             </div>
             <div class="my-2"><span class="text-red">*</span> 请根据当前任务所选体检类型，下载对应模板后再上传</div>
             <div class="table-box">
-              <ProTable :columns="personInforColumns" :toolButton="false" :showSearchBtn="false" :data="[{ name: 'aaaaaakaskhaskhahadhsa,d' }]">
+              <ProTable :columns="personInforColumns" :toolButton="false" :data="[{ name: 'aaaaaakaskhaskhahadhsa,d' }]">
                 <template #operation="scope">
                   <el-button type="primary" link>查看</el-button>
                   <el-button type="danger" link>删除</el-button>
@@ -70,10 +74,8 @@
 <script setup lang="tsx">
 import { ref, provide, reactive } from "vue";
 import Grid from "@/components/Grid/index.vue";
-import SearchFormItem from "@/components/TableSearchComponent/SearchForm/components/SearchFormItem.vue";
-import GridItem from "@/components/Grid/components/GridItem.vue";
 // import { ArrowUpBold } from "@element-plus/icons-vue/dist/types";
-
+const isActive = ref(1)
 const dateValue = ref('')
 const formValue = ref({
 })
@@ -164,13 +166,55 @@ const formColumns = ref<any>([
 </script>
 
 <style scoped lang="scss">
+.el-card {
+  // border-radius: 0px;
+  &.is-always-shadow {
+    box-shadow: none
+  }
+}
 .list_card {
-  height: calc(100vh - 255px);
+  height: calc(100vh - 240px);
   overflow: auto;
+  .card_item {
+    width: 100%;
+    margin-bottom: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-style: solid;
+    border-color: #E6EAEF;
+    border-radius: 4px;
+    border-width: 0px 0px 1px 0px;
+    font-size: 14px;
+    cursor: pointer;
+    &:hover {
+      border-color: #F1F5FB;
+      background: #F1F5FB;
+    }
+    &.active {
+      border-color: #F1F5FB;
+      background: #F1F5FB;
+    }
+    .el-checkbox.el-checkbox--large {
+      height: auto;
+    }
+  }
 }
 
 .content {
   height: calc(100vh - 90px);
   overflow: auto;
 }
+.card_title {
+    display: flex;
+    align-items: center;
+    span {
+      width: 4px;
+      height: 16px;
+      margin-right: 4px;
+      border-radius: 2px;
+      font-weight: bold;
+      background: #FF8F33;
+    }
+  }
 </style>
