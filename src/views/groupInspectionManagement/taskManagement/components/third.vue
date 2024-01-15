4bbbe84f79864c48c8678d0862edd26b352a40bb
喻进 <1972527753@qq.com>
2024-01-15 15:59:55 +0800

    feat:团检任务代码提交

---
+++ src/views/groupInspectionManagement/taskManagement/components/third.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div style="display:flex;align-items:center;justify-content:space-between">
+      <div>* 请根据当前任务所选体检类型，下载对应模板后再上传</div>
+      <div>
+        <el-button class="button" @click="handleLook(scope.$index)">下载模板</el-button>
+        <el-button class="button" @click="handleLook(scope.$index)">批量导入</el-button>
+      </div>
+    </div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="序号" />
+      <el-table-column prop="date" label="姓名" />
+      <el-table-column prop="date" label="年龄" />
+      <el-table-column prop="date" label="身份证号" />
+      <el-table-column prop="date" label="项目分组" />
+      <el-table-column prop="date" label="在岗类型" />
+      <el-table-column prop="date" label="体检状态" />
+      <el-table-column prop="address" label="操作" width="170">
+        <template #default="scope">
+          <el-button class="button" @click="handleLook(scope.$index)">查看</el-button>
+          <el-button class="button" @click="handleDel(scope.$index)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script setup lang="tsx" name="third">
+const tableData=ref([{}])
+const handleDel=()=>{
+
+}
+const handleLook=()=>{
+
+}
+</script>
--- src/views/groupInspectionManagement/taskManagement/index.vue
+++ src/views/groupInspectionManagement/taskManagement/index.vue
@@ -10,22 +10,22 @@
       <el-select v-model="form.value" placeholder="请选择单位" class="left-select">
         <el-option v-for="item in form.options" :key="item.value" :label="item.label" :value="item.value" />
       </el-select>
-      <el-date-picker v-model="form.value1" type="daterange" range-separator="至" start-placeholder="开始时间"
-        end-placeholder="结束时间" style="width: 100%;margin-top: 10px;" />
+      <el-date-picker
+        v-model="form.value1"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        style="width: 100%;margin-top: 10px;"
+      />
       <div class="left-input">
         <el-input v-model="form.value" placeholder="请输入任务名称" suffix-icon="Search" />
         <el-button class="button">重置</el-button>
       </div>
       <div class="left-box">
-        <div class="box">
-          <span>体检任务:</span> 测试
-        </div>
-        <div class="box">
-          <span>单位:</span> 测试
-        </div>
-        <div class="box">
-          <span>签订日期:</span> 测试
-        </div>
+        <div class="box"><span>体检任务:</span> 测试</div>
+        <div class="box"><span>单位:</span> 测试</div>
+        <div class="box"><span>签订日期:</span> 测试</div>
         <div class="group-type">
           <span class="zhi">职</span>
           <!-- <span class="jian" v-else-if="item.zyb === '0'">健</span>
@@ -35,39 +35,88 @@
     </el-card>
 
     <el-card shadow="hover" class="right">
-      <div class="rwtitle"><span>任务详情</span>
-        <div><el-button class="button" type="primary">编辑任务</el-button><el-button class="button"
-            type="primary">删除任务</el-button>
-        </div>
+      <div class="rwtitle">
+        <span>任务详情</span>
+        <div><el-button class="button" type="primary">编辑任务</el-button><el-button class="button" type="primary">删除任务</el-button></div>
       </div>
       <div><span>基本信息</span></div>
       <!-- 查询表单 -->
-      <SearchForm :search="_search" :reset="_reset" :columns="searchColumns" :search-param="searchParam" :search-col="3">
-        <template #username>
-          <el-select v-model="form.value" placeholder="请选择单位" class="left-select">
-            <el-option v-for="item in form.options" :key="item.value" :label="item.label" :value="item.value" />
-          </el-select>
-        </template>
-      </SearchForm>
+      <form-guid :fields="searchColumns" :model="form" :rules="rules" :col-count="4" :preview="true" />
+      <div><span>体检项目信息</span></div>
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane label="分组管理" name="first">
+          <Frist />
+        </el-tab-pane>
+        <el-tab-pane label="套餐/项目选择" name="second">
+          <Second />
+        </el-tab-pane>
+        <el-tab-pane label="人员管理" name="third">
+          <Third />
+        </el-tab-pane>
+        <el-tab-pane label="委托协议" name="fourth"><Fourth /></el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script setup lang="tsx" name="taskManagement">
+import type { TabsPaneContext } from 'element-plus'
+import Frist from '@/views/groupInspectionManagement/taskManagement/components/frist.vue'
+import Second from '@/views/groupInspectionManagement/taskManagement/components/second.vue'
+import Third from '@/views/groupInspectionManagement/taskManagement/components/third.vue'
+import Fourth from '@/views/groupInspectionManagement/taskManagement/components/fourth.vue'
 const form = reactive({
   value: '',
   options: [],
-  value1: '',
-})
-const searchColumns = ref([{ prop: "username", label: "用户姓名", search: { el: 'input', props: { placeholder: "请输入性别查询", filterable: true, remote: true, reserveKeyword: true, } } }, { prop: "username", label: "用户姓名", search: { el: 'input', props: { placeholder: "请输入性别查询", filterable: true, remote: true, reserveKeyword: true, } } }, { prop: "username", label: "用户姓名", search: { el: 'input', props: { placeholder: "请输入性别查询", filterable: true, remote: true, reserveKeyword: true, } } }, { prop: "username", label: "用户姓名", search: { el: 'input', props: { placeholder: "请输入性别查询", filterable: true, remote: true, reserveKeyword: true, } } },])
-const searchParam = reactive({})
-const _search = () => {
-
-};
-
-const _reset = () => {
-
-};
+  value1: ''
+});
+const activeName=ref('first')
+const searchColumns = ref([
+{
+    label: '任务名称',
+    name: 'name',
+    component: 'Input',
+  },
+  {
+    label: '团检单位',
+    name: 'idCard',
+    component: 'Select'
+  },
+  {
+    label: '体检类型',
+    name: 'peCode',
+    component: 'Select'
+  },
+  {
+    label: '签订日期',
+    name: 'groupId',
+    component: 'Datepicker',
+  },
+  {
+    label: '开始日期',
+    name: 'ksrq',
+    component: 'Datepicker',
+  },
+  {
+    label: '结束日期',
+    name: 'jsrq',
+    component: 'Datepicker',
+  },
+  {
+    label: '收费类型',
+    name: 'sflx',
+    component: 'Select',
+  },
+  {
+    label: '是否审核',
+    name: 'sfsh',
+    component: 'Select',
+  },
+]);
+const rules=ref([])
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
 </script>
 
 <style scoped lang="scss">
