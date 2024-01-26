33850362e3e667010846358feb158c0afa28e8dd
fengdanyue <549721719@qq.com>
2024-01-26 09:56:57 +0800

    feat:取消登记和体检花名册页面绘制

--- src/views/deskRegistration/roster/index.vue
+++ src/views/deskRegistration/roster/index.vue
@@ -22,34 +22,39 @@
       <template #tableHeader="scope">
         <div class="flex justify-between">
           <div>
-            <el-button type="primary" round>再次登记</el-button>
+            <el-button type="primary" round @click="showDialog = true">自动合并档案</el-button>
+            <el-button type="primary" round @click="showDialog = true">选择合并档案</el-button>
           </div>
         </div>
       </template>
     </ProTable>
+    <el-dialog title="选择合并档案" v-model="showDialog">
+      <div class="no-card">
+        <ProTable :columns="recordColumns" :toolButton="false" :data="[{ name: 'aaaaaakaskhaskhahadhsa,d' }]" :pagination="false"></ProTable>
+      </div>
+      <div class="w-full flex justify-center mt-18px">
+        <el-button round @click="showDialog = false">取消</el-button>
+        <el-button type="primary" round @click="showDialog = false">确定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
-<script setup name="deregistration" lang="tsx">
-import { ref } from 'vue';
-const tableColumns = ref([
+<script setup name="roster" lang="tsx">
+import { reactive, ref } from 'vue';
+const showDialog = ref(false)
+const tableColumns = reactive([
   { type: 'selection', },
   { prop: 'name', label: '体检号', search: { el: 'input' }, isShow: false },
   { prop: 'name', label: '姓名', search: { el: 'input', }, isShow: false, slot: 'xm' },
   { prop: 'name', label: '体检日期', search: { el: 'date-picker', props: { type: 'daterange' } }, isShow: false },
   { prop: 'name', label: '体检类型', search: { el: 'select' }, isShow: false },
   { prop: 'name', label: '业务类别', search: { el: 'select' }, isShow: false },
-  { prop: 'name', label: '回收', search: { el: 'select' }, isShow: false },
   { prop: 'name', label: '档案号', search: { el: 'input' }, isShow: false },
-  { prop: 'name', label: '单位', search: { el: 'select' }, isShow: false },
-  { prop: 'name', label: '终检日期', search: { el: 'date-picker', props: { type: 'daterange' } }, isShow: false },
-  { prop: 'name', label: '体检状态', search: { el: 'select' }, isShow: false },
-  { prop: 'name', label: '体检类别', search: { el: 'select' }, isShow: false },
   { prop: 'name', label: '身份证号', search: { el: 'input' }, isShow: false },
+  { prop: 'name', label: '电话', search: { el: 'input' }, isShow: false },
+  { prop: 'name', label: '单位', search: { el: 'input' }, isShow: false },
   { prop: 'name', label: '任务', search: { el: 'input' }, isShow: false },
-  { prop: 'name', label: '支付日期', search: { el: 'date-picker', props: { type: 'daterange' } }, isShow: false },
-  { prop: 'name', label: '创建人', search: { el: 'input' }, isShow: false },
-  { prop: 'name', label: '人员类别', search: { el: 'select' }, isShow: false },
   { prop: 'name', label: '体检号', fixed: 'left' },
   { prop: 'name', label: '档案号', fixed: 'left' },
   { prop: 'name', label: '证件号', fixed: 'left' },
@@ -60,29 +65,37 @@ const tableColumns = ref([
   { prop: 'name', label: '年龄' },
   { prop: 'name', label: '电话', },
   { prop: 'name', label: '体检日期', width: 100 },
-  { prop: 'name', label: '回收', },
-  { prop: 'name', label: '冻结', },
   { prop: 'name', label: '体检状态', width: 100 },
-  { prop: 'name', label: '需要总检', width: 100 },
-  { prop: 'name', label: '总费用', },
-  { prop: 'name', label: '团费', },
-  { prop: 'name', label: '个费', },
   { prop: 'name', label: '单位', },
   { prop: 'name', label: '分组', },
-  { prop: 'name', label: '部门', },
-  { prop: 'name', label: '备注', },
-  { prop: 'name', label: '介绍人', },
-  { prop: 'name', label: '创建人', },
-  { prop: 'name', label: '报到人', },
   { prop: 'name', label: '总检医生', width: 100 },
   { prop: 'name', label: '总检时间', width: 100 },
-  { prop: 'name', label: '审核医生', width: 100 },
-  { prop: 'name', label: '审核时间', width: 100 },
-  { prop: 'name', label: '完成时间', width: 100 },
-  { prop: 'name', label: '取消登记时间', width: 120 },
+  { prop: 'name', label: '旧档案号', width: 100 },
+  { prop: 'name', label: '合并时间', width: 100 },
+  { prop: 'name', label: '合并操作人', width: 100 },
+])
+const recordColumns = ref([
+  { type: 'selection', },
+  { prop: 'name', label: '档案号' },
+  { prop: 'name', label: '体检号' },
+  { prop: 'name', label: '姓名' },
+  { prop: 'name', label: '创建时间' },
 ])
 const options = ref<any>([])
 
 </script>
 <style scoped lang="scss">
+.color_ydy {
+  width: 8px;
+  height: 8px;
+  background-color: purple;
+}
+
+.color_bdj {
+  background-color: red;
+}
+
+:deep(.header-button-lf) {
+  width: 100%;
+}
 </style>
