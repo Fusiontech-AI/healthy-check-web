68a3acd7ed445c5aef6590da1f5db8ab4e7fac5b
tangmiao <1193017880@qq.com>
2024-02-28 18:01:34 +0800

    feat: 添加字典数据

---
+++ src/views/system/dict/data.tsx
@@ -0,0 +1,35 @@
+// 字典示例
+export const dictSampleList = [
+  {
+    title: '示例1',
+    style: 'background:#FFF3E0;color:#FFA81C;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例2',
+    style: 'background:#E5EFFF;color:#2175FF;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例3',
+    style: 'background:#EFEDFF;color:#8A7AFF;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例4',
+    style: 'background:#DEF9FC;color:#21CDE4;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例5',
+    style: 'background:#E1F2FF;color:#159DFF;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例6',
+    style: 'background:#FFEBEB;color:#F96E6E;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例7',
+    style: 'background:#EAF7D9;color:#66A712;text-align:center;padding:4px 8px;border-radius:2px'
+  },
+  {
+    title: '示例8',
+    style: 'background:#DDFAEC;color:#38D497;text-align:center;padding:4px 8px;border-radius:2px'
+  }
+];
